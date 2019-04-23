3af7d0b784079bbd3bbf9cfd21e8f0454812c720
Aayush Malhotra <37381075+AadumKhor@users.noreply.github.com>
2019-04-23 18:11:20 +0530

    Progress

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,99 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+typedef Tabchange = Function(int, bool);
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class NavigationBar extends StatefulWidget {
+  Key key;
+  @required
+  Color bgcolor;
+  final Tabchange tabchange;
+  final List<BottomNaviItem> items;
+  final double elevation;
+
+  NavigationBar(
+      {this.key,
+      this.bgcolor = Colors.white,
+      this.elevation = 20.0,
+      this.tabchange,
+      this.items})
+      : assert(items != null && items.length > 0),
+        assert(tabchange == null),
+        super(key: key);
+
+  @override
+  _NavigationBarState createState() => _NavigationBarState();
+}
+
+class _NavigationBarState extends State<NavigationBar> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return Container();
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class BottomNaviItem {
+  String title;
+  Color color;
+  String icon;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  BottomNaviItem({this.title, this.color, this.icon}) : super();
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  _BottomNaviItemState create(int index, Tabchange tabChange,
+          _NavigationBarState parentState, double titleWidth) =>
+      _BottomNaviItemState(
+        title: this.title,
+        icon: this.icon,
+        backgroudColor: this.color,
+        index: index,
+        tabChange: tabChange,
+        parentState: parentState,
+        maxTitleWidth: titleWidth,
+      );
+}
 
+class _BottomNaviItemState extends StatefulWidget {
   final String title;
+  final String icon;
+  final Color backgroudColor;
+  final int index;
+  final Tabchange tabChange;
+  final _NavigationBarState parentState;
+  final double maxTitleWidth;
+
+  _BottomNaviItemState({
+    Key key,
+    @required this.title,
+    @required this.icon,
+    @required this.backgroudColor,
+    @required this.index,
+    @required this.tabChange,
+    @required this.parentState,
+    @required this.maxTitleWidth,
+  })  : assert(title != null),
+        assert(icon != null),
+        assert(backgroudColor != null),
+        assert(index >= 0),
+        assert(tabChange != null),
+        assert(parentState != null),
+        assert(maxTitleWidth > 0),
+        super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  __BottomNaviItemStateState createState() => __BottomNaviItemStateState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class __BottomNaviItemStateState extends State<_BottomNaviItemState>
+    with AutomaticKeepAliveClientMixin, TickerProviderStateMixin {
+  bool _isOpen = false;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  set isOpen(bool isOpen){}
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+  // TODO: implement wantKeepAlive
+  bool get wantKeepAlive => null;
 }
